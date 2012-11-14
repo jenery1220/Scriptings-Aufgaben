973cee395149f3209820b73b17e902df76fb8c3e
Yan Feng <jenery1220@qq.com>
2012-11-14 16:29:37 +0100

    Yans Semester

--- Semester-Objekt/Semester.js
+++ Semester-Objekt/Semester.js
@@ -3,6 +3,52 @@
 // =============================================================
 
 var mein_semester = {
-  // bilde hier dein Semester ab!
+  student: {
+	name:"Yan Feng",
+	studiegang: "Interface Design",
+	studiesemester: 2,
+	welches: "WS",
+    jahr: "2012/2013",
+	},
+
+  veranstaltung:[
+   { 
+	titel: "Scriptings",
+    sws: 2,
+    credits:3,
+    workload:90
+    },
+   {
+	title:"Read & Write",
+	sws:2,
+	credits:3,
+    workload:90
+    },
+   {
+	title:"vvvv",
+	sws:2,
+	credits:2,
+    workload:90
+	},
+   {
+	title:"Proberaum",
+	sws:2,
+	credits:2,
+    workload:90
+	},
+   {
+	title:"Aesthetik der Globalisierung",
+	sws:2,
+	credits:2,
+    workload:90
+	}
+   ]
 };
 
+JSON.stringify(mein_semester);
+//"{"student":{"name":"Yan Feng","studiegang":"Interface //Design","studiesemester":2,"welches":"WS","jahr":"2012/2013"},"veranstaltung":
+//[{"titel":"Scriptings","sws":2,"credits":3,"workload":90},
+//{"title":"Read & Write","sws":2,"credits":3,"workload":90},
+//{"title":"vvvv","sws":2,"credits":2,"workload":90},
+//{"title":"Proberaum","sws":2,"credits":2,"workload":90},
+//{"title":"?sthetik der Globalisierung","sws":2,"credits":2,"workload":90}]}"
